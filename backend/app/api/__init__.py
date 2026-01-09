1cf6f7ca02f719579580b2e2311a8516649fafe9
tiroq <isshamray@gmail.com>
2026-01-09 05:36:42 +0000

    Add artifacts router to API package exports

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -3,5 +3,6 @@
 from .projects import router as projects_router
 from .tasks import router as tasks_router
 from .change_requests import router as change_requests_router
+from .artifacts import router as artifacts_router
 
-__all__ = ["projects_router", "tasks_router", "change_requests_router"]
+__all__ = ["projects_router", "tasks_router", "change_requests_router", "artifacts_router"]
